7075645e5a36ccbad52b897d099dfa530a293a1b
sjhang <sonik.jhang@utexas.edu>
2017-09-22 21:13:02 -0500

    read me and back to the original recursive solution

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# worksmith-coding-challenge
+# Worksmith Coding Challenge
+
+##Simple Make Change application
+
+A program for making change. This program takes as an input the amount of change to make (e.g.   4.50), and it output the number of each coin to dispense so that the fewest possible coins are dispensed. The available coins are quarters, dimes, nickels, and pennies.
+
+![alt text](assets/splash.png)
+![alt text](assets/splash_with_results.png)
+
+## How to use
+
+1. Clone the repo
+2. Double click *index.html* from the directory
+3. Type in desired change amount in *dollars*
+4. Press *Submit* !
+(optional)
+5. Type in different cents value for fun or uncheck boxes to see different results!
+
+### Challenge
+
+makeChange function implemented right now immediately uses the biggest coin possible. This function will not properly work for example like $.14 with a unique coin of worth $.07 along with other standard US coins.
--- main.js
+++ main.js
@@ -2,6 +2,8 @@ window.onload = () => {
   const totalAmount = document.querySelector('.inputAmount');
   const chboxButtons = document.querySelectorAll('.checkbox-button');
   const submitForm = document.querySelector('form.panel-body');
+  const displayResults = document.querySelector('.results');
+  const resultPanel = displayResults.parentNode;
 
   const validateTotal = input => {
     // regex match to validate input amount
@@ -18,32 +20,65 @@ window.onload = () => {
       case 'nickel': return 5;
       case 'penny': return 1;
       case 'optional':
-        let value = coin.nextElementSibling.value;
+        let value = parseFloat(coin.nextElementSibling.value);
         return (
           value ?
           (
             (value <= .99 && value >= 0.02 && value != .25 || value != .10 || value != .05) ?
-            value * 100 : alert('wrong value')
+            Math.floor(value * 100) : alert('wrong value')
           ) :
           alert('insert value')
         )
     }
   }
 
+  const makeChange = (total, coins, memo) => {
+    // this method relies on coins being in descending order
+    let change = []
+
+    // immediately uses the biggest coin possible
+    let firstCoin = coins[0];
+    let count = Math.floor(total / firstCoin);
+    for (var i = 0; i < count; i++) {
+      change.push(firstCoin);
+    }
+    total = total - (count * firstCoin);
+
+    if (total > 0) {
+      change = change.concat(makeChange(total, coins.slice(1), memo))
+    }
+
+    return change
+  }
+
+/*
+  // upgraded version of the previous makeChange function, however, in a recursive function in javascript,
+  // it seems to be impossible to use continue.
+  // see https://stackoverflow.com/questions/31096483/illegal-continue-javascript
+
   const makeChange = (total, coins) => {
     if (total === 0) return [];
-    // this method relies on coins being in descending order
     let bestChange = null;
     let change = []
 
+    // coins.forEach(coin => {
+    //   // loops through with a coin until the coin amount is greater than the total amount
+    //   if (coin > total) return;
+    //   // once the coin > total, move on to the next coin
+    //   total = total - coin;
+    //   let changeForRest = makeChange(total, coins);
+    //   change = [coin].concat(changeForRest);
+    //
+    //   if (!bestChange || change.length < bestChange.length) {
+    //     bestChange = change;
+    //   }
+    // })
+
     for (var i = 0; i < coins.length; i++) {
       let coin = coins[i];
-      debugger;
       if (coin > total) {
-        debugger
         continue;
       } else {
-        debugger;
         let changeForRest = makeChange(total-coin, coins);
         change = [coin].concat(changeForRest);
       }
@@ -55,23 +90,45 @@ window.onload = () => {
 
     return bestChange
   }
+  */
 
+  const displayCoinCounts = (changes, coins) => {
+    coins.forEach(coin => {
+      let count = changes.filter(change => coin === change).length;
+      displayResults.innerText += `${count}  $ ${coin/100}\n`
+    })
+  }
   // runs when submit button is hit
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // reset result panel
+    if (displayResults.innerText) displayResults.innerText = "";
+    resultPanel.classList.remove('panel-success');
+    resultPanel.classList.add('panel-default');
+
     let coins = [];
     let total = validateTotal(totalAmount);
-    total = total * 100; // for easier computation, change total in cents
+    total = Math.floor(total * 100); // for easier computation, change total in cents
 
     chboxButtons.forEach(btn => {
       if (btn.checked) coins.push(coinAmount(btn));
     })
+
+    // sorts coins array in descending order
     coins.sort((a,b) => b-a);
-    let a = makeChange(total, coins)
-    debugger;
+    let changes = makeChange(total, coins)
+
+    // render results
+    if (changes.length) {
+      // change the color of the panel
+      resultPanel.classList.remove('panel-default');
+      resultPanel.classList.add('panel-success');
+      displayCoinCounts(changes, coins);
+    }
+
     return false;
   }
-
 
   submitForm.addEventListener("submit", handleSubmit);
 }
